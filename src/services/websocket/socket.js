000755a3d1d08c1a76ad5576374eef6e77df89de
Furano <viperbach.18@gmail.com>
2021-01-26 23:09:05 -0300

    fix .env and baseUrl

--- src/services/websocket/socket.js
+++ src/services/websocket/socket.js
@@ -1,7 +1,7 @@
 import io from 'socket.io-client'
 import { getToken } from '../auth'
 
-const socket = io('http://localhost:3001', {
+const socket = io('https://chat-beckend.herokuapp.com', {
   transports: ['websocket'],
   query: {
     auth: `Bearer ${getToken()}`
